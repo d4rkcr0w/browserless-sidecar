b61a3f5bb53c7d892924360f6d8932c97224d22a
xudongdev <me@xudong.dev>
2021-11-02 13:16:33 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Install dependencies only when needed
+FROM node:16-alpine AS deps
+WORKDIR /app
+
+COPY package.json package-lock.json ./
+
+RUN npm ci
+
+# Production image, copy all the files and run next
+FROM node:16-alpine AS runner
+WORKDIR /app
+
+ENV NODE_ENV production
+
+COPY --from=deps /app/node_modules ./node_modules
+COPY --from=deps /app/package.json ./package.json
+
+EXPOSE 9000
+
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,69 @@
+import "dotenv/config";
+
+import Axios from "axios";
+import pino from "pino";
+import Lightship from "lightship";
+
+const logger = pino();
+
+const axios = Axios.create({
+  baseURL: process.env.BROWSERLESS_URL || "http://browserless:3000",
+});
+
+const lightship = Lightship.createLightship();
+
+const bootstrap = async () => {
+  while (true) {
+    logger.info("running readiness probe");
+
+    let response;
+
+    try {
+      response = await axios("/pressure");
+    } catch (error) {
+      logger.error(
+        { err },
+        "cannot get the latest prssure metrics; signaling that instance is not ready"
+      );
+
+      lightship.signalNotReady();
+    }
+
+    if (response) {
+      const { pressure } = response.data;
+
+      logger.info(
+        {
+          pressure,
+        },
+        "received new pressure report"
+      );
+
+      if (!pressure.isAvailable) {
+        logger.warn(
+          "pressure API indicates that instance is not ready; signaling that instance is not ready"
+        );
+        lightship.signalNotReady();
+      } else if (
+        pressure.running >=
+        Number(process.env.MAX_CONCURRENT_SESSIONS || pressure.maxConcurrent)
+      ) {
+        logger.warn(
+          "pressure API reported queue size is equal or greater than the maximum desired queue size; signaling that instance is not ready"
+        );
+        lightship.signalNotReady();
+      } else {
+        logger.info(
+          "instance is ready and can handle more requests; signaling that instance is ready"
+        );
+        lightship.signalReady();
+      }
+    }
+
+    await new Promise((resolve) =>
+      setTimeout(resolve, Number(process.env.CHECK_INTERVAL || 5000))
+    );
+  }
+};
+
+bootstrap();
